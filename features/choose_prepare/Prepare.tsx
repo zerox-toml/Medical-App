88b201954551448d64e99210e1237095805a7cd3
spectrecoder <pong96111kind@gmail.com>
2024-08-11 06:54:37 -0700

    update:prepare span

--- features/choose_prepare/Prepare.tsx
+++ features/choose_prepare/Prepare.tsx
@@ -581,7 +581,7 @@ const Prepare = ({ isProductStep, isStep, setIsStep, setIsProductStep, showFilte
                   )) : <span className=" w-full">Es wurden keine Produkte gefunden. Bitte ändern Sie die Filtereinstellungen und versuchen Sie es erneut.</span>
                 // )
               }
-              {isMoreAvailable &&
+              {isMoreAvailable &&  
                 <span
                   onClick={showMoreProduct}
                   className=" text-[16px] text-[#41057E] underline mt-3 cursor-pointer"
